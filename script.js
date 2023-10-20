83c93b2fa7ca66d954eb6b5d49b0572a9a790467
MunchingKitten <139931826+MunchingKitten@users.noreply.github.com>
2023-10-19 21:29:45 -0400

    Made password error and overall layout better

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.querySelectorAll()
+let password = document.querySelector('#password');
+let confPassword = document.querySelector('#conf-password');
+let passwordError = document.querySelector('#password-error');
+
+
+const confirmPassword = () => {
+    if (password.value != confPassword.value) {
+        passwordError.textContent = "* Passwords do not match";
+        password.style.borderColor = 'red';
+        confPassword.style.borderColor = 'red';
+        console.log('items should change');
+    }
+    else console.log('no change');
+}
+
+let sumbit = document.querySelector('#submit-button');
+sumbit.addEventListener('click', confirmPassword);
